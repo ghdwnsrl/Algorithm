1dcbf56f094b813fa9f00ae685768259a3b2cf6e
ghdwnsrl <ghdwnsrl1991@gmail.com>
2024-10-07 20:19:54 +0900

    [level 1] Title: 소수 찾기, Time: 21.16 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -2,26 +2,24 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        boolean[] prime = new boolean[n + 1];
-
+        boolean[] prime = new boolean[n+1];
         prime[0] = true;
         prime[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        
+        for(int i = 2; i <= Math.sqrt(n); i++) {
             if (prime[i]) {
                 continue;
             }
-
-            for (int j = i * i; j < prime.length; j=j+i) {
+            for(int j = i * i; j < prime.length; j += i) {
                 prime[j] = true;
             }
         }
         
-        for (int i = 0; i < prime.length; i++) {
-            if (!prime[i]) {
+        for(int i = 2; i < prime.length; i ++) {
+            if(!prime[i])
                 answer++;
-            }
         }
+        
         return answer;
     }
 }
