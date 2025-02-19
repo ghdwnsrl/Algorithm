82b9f9d526c2ed4c4fd9cc4ea5f5017c947fad5a
ghdwnsrl <ghdwnsrl1991@gmail.com>
2025-02-19 18:05:26 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.56 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -4,18 +4,10 @@ class Solution {
     public int solution(int left, int right) {
         return IntStream.rangeClosed(left, right)
                 .map(i -> {
-                    int count = 0;
-                    double sqrt = Math.sqrt(i);
-                    for (int j = 1; j <= sqrt; j++) {
-                        if (i % j == 0) {
-                            count++;
-                            if (j != i / j) {
-                                count++;
-                            }
-                        }
-                    }
-                    if (count % 2 == 0) return i;
-                    else return i * -1;
+                    if (i % Math.sqrt(i) == 0)
+                        return i * -1;
+                    else
+                        return  i;
                 })
                 .sum();
     
