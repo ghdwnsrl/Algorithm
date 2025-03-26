0c9c14045996a5d04d9f32ac2761c6f20956ecdb
ghdwnsrl <ghdwnsrl1991@gmail.com>
2025-03-26 18:41:44 +0900

    [level 2] Title: 주식가격, Time: 26.43 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,11 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        Deque<int[]> stack = new ArrayDeque<>();
+
         for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j]) break;
+            answer[i] = prices.length - i - 1;
+            int[] curP = new int[]{i, prices[i]};
+            while(!stack.isEmpty() && curP[1] < stack.peek()[1]) {
+                int[] p = stack.pop();
+                answer[p[0]] = i - p[0];
             }
+            stack.push(curP);
         }
         return answer;
     }
