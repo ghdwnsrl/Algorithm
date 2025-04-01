deccd0e9ec45eb2d9bda40aa2ceb53f848b5b8dc
ghdwnsrl <ghdwnsrl1991@gmail.com>
2025-04-01 15:16:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.39 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,34 +4,39 @@ class Solution {
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
     public int solution(int[][] maps) {
-        int answer = 0;        
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-
+        boolean[][] visited  = new boolean[maps.length][maps[0].length];
         return bfs(maps, visited);
     }
     
     static int bfs(int[][] maps, boolean[][] visited) {
+        int answer = -1;
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0,0,1});
         visited[0][0] = true;
-        int answer = -1;
-        while (!queue.isEmpty()) {
+        
+        while(!queue.isEmpty()) {
             int[] cur = queue.poll();
-            if (cur[0] == maps.length - 1 && cur[1] == maps[0].length - 1) {
-                answer = cur[2];
-                return answer;
+            
+            // 상대방 진영 도착
+            if(cur[0] == maps.length - 1 && cur[1] == maps[0].length - 1) {
+                return cur[2];
             }
-            for (int i = 0; i < 4; i++) {
-                int nextX = cur[0] + dx[i];
-                int nextY = cur[1] + dy[i];
-                if (nextX >= 0 && nextX < maps.length && nextY >= 0 && nextY < maps[0].length) {
-                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
-                        visited[nextX][nextY] = true;
-                        queue.offer(new int[]{nextX, nextY, cur[2] + 1});
+            
+            for(int i = 0; i < 4; i++) {
+                int nX = cur[0] + dx[i];
+                int nY = cur[1] + dy[i];
+                
+                // 범위 내 
+                if(nX >= 0 && nX < maps.length && nY >= 0 && nY < maps[0].length) {
+                    if(!visited[nX][nY] && maps[nX][nY] == 1) {
+                        visited[nX][nY] = true;
+                        queue.offer(new int[]{nX, nY, cur[2] + 1});
                     }
                 }
             }
         }
+        
         return answer;
     }
 }
